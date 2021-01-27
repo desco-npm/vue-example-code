bf8c7a85e6899f14e451e8c0b64d10257dbe87e8
eudiasrafael <eu@diasrafael.com.br>
2021-01-27 18:01:12 -0300

    1.1.0

--- index.vue
+++ index.vue
@@ -1,19 +1,26 @@
 <template lang="pug">
   div.example-code
-    el-tabs(type="border-card")
-      el-tab-pane(v-for="(Code, k) in codes" :key="k")
-        span(slot="label")
-          i(:class="getTabIcon(Code)")
-          | {{getTabName(Code, k + 1)}}
-        pre(v-html="getCodeContent(Code)")
+    Codes(v-if="!code.example" :codes="codes")
+    el-card(v-else)
+      div.header(slot="header")
+        |{{code.title}}
+      |{{code.description}}
+      Codes(:codes="codes" :class="{ show: codeShow, }")
+      el-divider
+        i(
+          :class="{ 'el-icon-arrow-down': !codeShow, 'el-icon-arrow-up': codeShow, }"
+          @click="toogleCodeShow"
+        )
 </template>
 
 <script>
   import isArray from 'is-array'
-  import Syntax from "syntax"
+
+  import Codes from './Codes'
 
   export default {
     name: 'ExampleCode',
+    components: { Codes, },
     props: {
       code: Array,
     },
@@ -21,116 +28,14 @@
       return {
         codes: [],
         iconDefault: 'fas fa-laptop-code',
+        codeShow: false,
       }
     },
     methods: {
-      getCodeContent (Code) {
-        try {
-          let syntax = new Syntax({ language: this.getValidSyntaxLang(Code), })
-  
-          return syntax.richtext(Code.content).html()
-        }
-        catch (e) {
-          return Code.content
-        }
-      },
-      getValidSyntaxLang (Code) {
-        switch (Code.lang.toLowerCase()) {
-          case 'javascript':
-          case 'js': return 'js'
-          case 'ts':
-          case 'typescript': return 'typescript'
-          case 'html': 
-          case 'pug': 
-          case 'jade': return 'xml'
-          case 'css': 
-          case 'sass':
-          case 'scss': return 'css'
-          case 'less':
-          case 'lesscss': return 'less'
-          case 'bash': 
-          case 'prompt':
-          case 'cmd': 
-          case 'terminal': return 'bash'
-          // case 'env': return 'ini'
-          // case 'db': 
-          // case 'database':
-          // case 'sql': return 'bash'
-          default: return 'bash'
-        }
-      },
-      getTabIcon (Code) {
-        switch (Code.lang.toLowerCase()) {
-          case 'js':
-          case 'ts':
-          case 'javascript':
-          case 'typescript': return 'fab fa-js'
-          case 'html':
-          case 'pug': 
-          case 'jade': return 'fas fa-code'
-          case 'css': return 'fab fa-css3'
-          case 'sass':
-          case 'scss': return 'fab fa-sass'
-          case 'less':
-          case 'lesscss': return 'fab fa-less'
-          case 'bash':
-          case 'prompt':
-          case 'cmd': 
-          case 'terminal': return 'fas fa-terminal'
-          case 'env': return 'fas fa-list'
-          case 'db':
-          case 'database':
-          case 'sql': return 'fas fa-database'
-          default: return this.iconDefault
-        }
-      },
-      getCodeLang (Code) {
-        switch (Code.lang.toLowerCase()) {
-          case 'javascript':
-          case 'js': return 'JavaScript'
-          case 'ts':
-          case 'typescript': return 'TypeScript'
-          case 'html': return 'HTML'
-          case 'pug': return 'PUG'
-          case 'jade': return 'JADE'
-          case 'css': return 'CSS'
-          case 'sass':
-          case 'scss': return 'SASS'
-          case 'less':
-          case 'lesscss': return 'LESS'
-          case 'bash': return 'Bash'
-          case 'prompt': return 'Prompt'
-          case 'cmd': return 'Cmd'
-          case 'terminal': return 'Terminal'
-          case 'env': return 'ENV'
-          case 'db': return 'DB'
-          case 'database': return 'DataBase'
-          case 'sql': return 'SQL'
-          default: return undefined
-        }
-      },
-      getTabName (Code, k) {
-        const name = Code.name
-        const lang = this.getCodeLang(Code)
-        const nameExists = name !== undefined
-        const langExists = lang !== undefined
-
-        if (!nameExists && langExists) {
-          return lang
-        }
-
-        if (nameExists && !langExists) {
-          return `${name} (${Code.lang})`
-        }
-
-        if (nameExists) {
-          return name
-        }
-
-        return `Tab ${k} (${Code.lang})`
-      },
       fetch () {
-        this.codes = (isArray(this.code) ? this.code : [ this.code, ])
+        const code = this.code.example || this.code
+
+        this.codes = (isArray(code) ? code : [ code, ])
           .map(i => {
             i.content = i.content.split('\n')
 
@@ -146,7 +51,10 @@
 
             return i
           })
-      }
+      },
+      toogleCodeShow () {
+        this.codeShow = !this.codeShow
+      },
     },
     mounted () {
       this.fetch()
@@ -162,14 +70,34 @@
   }
 </script>
 
-<style lang="scss">
+<style lang="scss" scoped>
   .example-code {
     width: 100%;
+  }
+</style>
 
-    i,
-    svg,
-    svg path {
-      margin-right: 5px;
+<style lang="scss" scoped>
+  .example-code {
+    .el-card {
+      padding-bottom: 0;
+
+      .header {
+        line-height: 0;
+        font-weight: bold;
+      }
+
+      .el-tabs {
+        transition: all 2s 0s;
+        max-height: 0;
+        opacity: 0;
+        overflow: hidden;
+        margin-top: 1rem;
+
+        &.show {
+          max-height: 500px;
+          opacity: 1;
+        }
+      }
     }
   }
 </style>
